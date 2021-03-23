1742aa149990e04e967db3dda8adc14b8c78d2b2
Fahmi ITP <fahmi@itprovent.com>
2021-03-23 15:40:13 +0700

    Added Wait Confirmation Function, Added All User Tokens Page

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTdms3QCnS4UYTBdr9GUXPYjcTn9WfV_4",
+  authDomain: "itp-nft-demo.firebaseapp.com",
+  projectId: "itp-nft-demo",
+  storageBucket: "itp-nft-demo.appspot.com",
+  messagingSenderId: "836091965029",
+  appId: "1:836091965029:web:005bf724353b1377fe608c",
+  measurementId: "G-BXQYQYJHBY",
+};
+
+export { firebaseConfig };

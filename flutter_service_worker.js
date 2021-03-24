a5c26d2897f107b7ced4cd25c1019d181922e54c
Fahmi ITP <fahmi@itprovent.com>
2021-03-24 16:34:26 +0700

    Changed Image Upload Path from Contract plus Token ID to Contract Folder and Token ID

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "289b17e3257099c05cefa3b5678afe96",
-"index.html": "bc538f7bb80164f34b259568f24d4234",
-"/": "bc538f7bb80164f34b259568f24d4234",
-"main.dart.js": "dcc52b1f20da908b37c1c4ac0db0765d",
+"index.html": "36f81ecd82b9888468ecc5d23f478f12",
+"/": "36f81ecd82b9888468ecc5d23f478f12",
+"main.dart.js": "6206ec8b5815ecf2e0293c9bb19a9413",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
